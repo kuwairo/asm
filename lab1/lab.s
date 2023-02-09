962ce7c9f1627717eabe9ef6cae32364b0ba8dea
Ilya S <sewd33p@gmail.com>
2023-02-09 13:48:17 +0700

    Custom function for division and printing

--- lab1/lab.s
+++ lab1/lab.s
@@ -5,15 +5,15 @@
 sfmtd:
 	.asciz "%ld %ld"
 pfmtd1:
-	.asciz "z = (xy - 1) / (x + y) = %ld.%03ld\n"
+	.asciz "z = (xy - 1) / (x + y) = %c%ld.%03ld\n"
 pfmtd2:
 	.asciz "z = x^3 + y - 1 = %ld.%03ld\n"
 pfmtd3:
-	.asciz "z = (xy + 1) / x^2 = %ld.%03ld\n"
+	.asciz "z = (xy + 1) / x^2 = %c%ld.%03ld\n"
 pfmtd4:
-	.asciz "z = (x + y) / (x - y) = %ld.%03ld\n"
+	.asciz "z = (x + y) / (x - y) = %c%ld.%03ld\n"
 pfmtd5:
-	.asciz "z = (3x^3 - 1) / x^3 = %ld.%03ld\n"
+	.asciz "z = (3x^3 - 1) / x^3 = %c%ld.%03ld\n"
 
 .section .text
 
@@ -22,7 +22,7 @@ pfmtd5:
 
 main:
 	pushq %rbp
-	mov %rsp, %rbp
+	movq %rsp, %rbp
 	subq $16, %rsp
 
 	movq $sfmtd, %rdi
@@ -42,26 +42,11 @@ main:
 	movq X(%rbp), %rbx
 	addq Y(%rbp), %rbx
 
-	# %rax <- (xy - 1) div (x + y)
-	# %rdx <- (xy - 1) rem (x + y)
-	cqo
-	idivq %rbx
-
-	movq %rax, %r15
-	imulq $1000, %rdx, %rax
-	cqo
-	idivq %rbx
-
-	cmpq $0, %rax
-	jge print1
-	negq %rax
-
-print1:
-	movq $pfmtd1, %rdi
-	movq %r15, %rsi
-	movq %rax, %rdx
+	movq %rax, %rdi
+	movq %rbx, %rsi
+	movq $pfmtd1, %rdx
 	xorq %rax, %rax
-	call printf
+	call printdiv
 
 	# (2) z = x^3 + y - 1
 
@@ -90,26 +75,11 @@ print1:
 	imulq Y(%rbp)
 	incq %rax
 
-	# %rax <- (xy + 1) div x^2
-	# %rdx <- (xy + 1) rem x^2
-	cqo
-	idivq %rbx
-
-	movq %rax, %r15
-	imulq $1000, %rdx, %rax
-	cqo
-	idivq %rbx
-
-	cmpq $0, %rax
-	jge print3
-	negq %rax
-
-print3:
-	movq $pfmtd3, %rdi
-	movq %r15, %rsi
-	movq %rax, %rdx
+	movq %rax, %rdi
+	movq %rbx, %rsi
+	movq $pfmtd3, %rdx
 	xorq %rax, %rax
-	call printf
+	call printdiv
 
 	# (4) z = (x + y) / (x - y)
 
@@ -121,26 +91,11 @@ print3:
 	movq X(%rbp), %rbx
 	subq Y(%rbp), %rbx
 
-	# %rax <- (x + y) div (x - y)
-	# %rdx <- (x + y) rem (x - y)
-	cqo
-	idivq %rbx
-
-	movq %rax, %r15
-	imulq $1000, %rdx, %rax
-	cqo
-	idivq %rbx
-
-	cmpq $0, %rax
-	jge print4
-	negq %rax
-
-print4:
-	movq $pfmtd4, %rdi
-	movq %r15, %rsi
-	movq %rax, %rdx
+	movq %rax, %rdi
+	movq %rbx, %rsi
+	movq $pfmtd4, %rdx
 	xorq %rax, %rax
-	call printf
+	call printdiv
 
 	# (5) z = (3x^3 - 1) / x^3
 
@@ -155,26 +110,11 @@ print4:
 	imulq $3, %rax
 	decq %rax
 
-	# %rax <- (3x^3 - 1) div x^3
-	# %rdx <- (3x^3 - 1) rem x^3
-	cqo
-	idivq %rbx
-
-	movq %rax, %r15
-	imulq $1000, %rdx, %rax
-	cqo
-	idivq %rbx
-
-	cmpq $0, %rax
-	jge print5
-	negq %rax
-
-print5:
-	movq $pfmtd5, %rdi
-	movq %r15, %rsi
-	movq %rax, %rdx
+	movq %rax, %rdi
+	movq %rbx, %rsi
+	movq $pfmtd5, %rdx
 	xorq %rax, %rax
-	call printf
+	call printdiv
 
 	xorq %rax, %rax
 	leave
---
+++ lab1/printdiv.s
@@ -0,0 +1,62 @@
+.globl printdiv
+.type printdiv, @function
+
+.section .text
+
+pos:
+	.quad 0 # ''
+neg:
+	.quad 45 # '-'
+
+.equ N, -8
+.equ M, -16
+.equ F, -24
+
+printdiv:
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $32, %rsp # 16 byte S.A.
+
+	movq %rdi, N(%rbp) # 1st op.
+	movq %rsi, M(%rbp) # 2nd op.
+	movq %rdx, F(%rbp) # fmt string
+
+	# %r8 <- sign(%rdi / %rsi)
+	shrq $63, %rdi
+	shrq $63, %rsi
+	xorq %rsi, %rdi
+	cmovzq pos, %r8
+	cmovnzq neg, %r8
+
+	# %r9 <- abs M(%rbp)
+	movq M(%rbp), %rax
+	cqo
+	xorq %rdx, %rax
+	subq %rdx, %rax
+	movq %rax, %r9
+
+	# %rax <- abs N(%rbp)
+	movq N(%rbp), %rax
+	cqo
+	xorq %rdx, %rax
+	subq %rdx, %rax
+
+	# %rax <- %rax div %r9
+	# %rdx <- %rax rem %r9
+	cqo
+	idivq %r9
+
+	movq %rax, %r10
+	imulq $1000, %rdx, %rax
+	cqo
+	idivq %r9
+
+	movq F(%rbp), %rdi
+	movq %r8, %rsi
+	movq %r10, %rdx
+	movq %rax, %rcx
+	xorq %rax, %rax
+	call printf
+
+	leave
+	ret
