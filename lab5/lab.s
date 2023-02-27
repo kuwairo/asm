5e5ee4ccb8bb5144e5fb39e7eb7f5d5857d3e428
Ilya S <sewd33p@gmail.com>
2023-02-28 01:17:34 +0700

    Lab 5: refactor

--- lab5/lab.s
+++ lab5/lab.s
@@ -5,7 +5,7 @@
 sfmt:
 	.asciz "%ld %ld"
 pfmt:
-	.asciz "y = (%ld) + (%ld) = %ld\n"
+	.asciz "y = %ld + (%ld) = %ld\n"
 
 .section .text
 
@@ -38,34 +38,31 @@ mloop:
 
 	# t0 <- if x > a then x mod 4 else a
 
+	bgt s1, s2, then1
 	mv t0, s2
-	mv t1, s1
-	bgt t1, t0, then1
 	j cend1
 then1:
-	li t2, 4
-	rem t0, t1, t2
+	li t1, 4
+	rem t0, s1, t1
 cend1:
 
 	# t1 <- if x / a > 3 then a * x else x
 
-	mv t2, s1
-	mv t3, s2
-	div t4, t2, t3
-	rem t5, t2, t3
+	div t1, s1, s2
+	rem t2, s1, s2
+	li t3, 3
 
-	li t6, 3
-	bgt t4, t6, then2
-	blt t4, t6, else2
-	beq t5, zero, else2
+	bgt t1, t3, then2
+	blt t1, t3, else2
+	beq t2, zero, else2
 then2:
-	mul t1, t2, t3
+	mul t1, s2, s1
 	j cend2
 else2:
-	mv t1, t2
+	mv t1, s1
 cend2:
 
-	add t2, t1, t0
+	add t2, t0, t1
 
 	la a0, pfmt
 	mv a1, t0
