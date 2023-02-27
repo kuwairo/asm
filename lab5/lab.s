d12861234b860f225f7384537952517c47bf8b24
Ilya S <sewd33p@gmail.com>
2023-02-28 00:34:58 +0700

    Lab 5: add a loop, use s-registers

--- lab5/lab.s
+++ lab5/lab.s
@@ -5,29 +5,41 @@
 sfmt:
 	.asciz "%ld %ld"
 pfmt:
-	.asciz "y = %ld + %ld = %ld\n"
+	.asciz "y = (%ld) + (%ld) = %ld\n"
 
 .section .text
 
-.equ RA, 16
-.equ X, 8
-.equ A, 0
+.equ RA, 24
+.equ S1, 16
+.equ S2, 8
+.equ S3, 0
 
 main:
-	addi sp, sp, -24
+	addi sp, sp, -32
 	sd ra, RA(sp)
+	sd s3, S3(sp)
 
 	la a0, sfmt
-	addi a1, sp, X
-	addi a2, sp, A
+	addi a1, sp, S1
+	addi a2, sp, S2
 	call scanf
 
-	# TODO: loop
+	ld t0, S1(sp)
+	ld t1, S2(sp)
+	sd s1, S1(sp)
+	sd s2, S2(sp)
+	mv s1, t0
+	mv s2, t1
+
+	mv s3, s1
+	addi s3, s3, 9
+
+mloop:
 
 	# t0 <- if x > a then x mod 4 else a
 
-	ld t0, A(sp)
-	ld t1, X(sp)
+	mv t0, s2
+	mv t1, s1
 	bgt t1, t0, then1
 	j cend1
 then1:
@@ -37,8 +49,8 @@ cend1:
 
 	# t1 <- if x / a > 3 then a * x else x
 
-	ld t2, X(sp)
-	ld t3, A(sp)
+	mv t2, s1
+	mv t3, s2
 	div t4, t2, t3
 	rem t5, t2, t3
 
@@ -61,6 +73,12 @@ cend2:
 	mv a3, t2
 	call printf
 
+	addi s1, s1, 1
+	ble s1, s3, mloop
+
 	ld ra, RA(sp)
-	addi sp, sp, 24
+	ld s1, S1(sp)
+	ld s2, S2(sp)
+	ld s3, S3(sp)
+	addi sp, sp, 32
 	ret
